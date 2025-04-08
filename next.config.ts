7e0b10b88457b4853d10cd42054d0a7f3bab3009
nguyendang03 <npmd2004@gmail.com>
2025-04-08 21:51:21 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  // output: 'standalone',
-  images: {
-    domains: ['your-domain.com'],
-  },
-  // experimental: {
-  //   appDir: true,
-  // },
-  // basePath: '/portfolio',
+  /* config options here */
 }
-
+ 
 module.exports = nextConfig
