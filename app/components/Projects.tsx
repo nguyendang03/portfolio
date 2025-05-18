2d6f1d7a0202d8c5b26ad679af57d5718077dd7d
nguyendang03 <121648651+nguyendang03@users.noreply.github.com>
2025-05-18 15:26:13 +0700

    Update Projects.tsx

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -40,7 +40,7 @@ export default function Projects() {
         <div className="text-center mb-16">
           <h2 className="text-3xl sm:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-600 to-purple-600">My Projects</h2>
           <p className="mt-4 text-lg text-gray-600 dark:text-gray-300 max-w-3xl mx-auto">
-            Here are some of my projects I have taken part in
+            Here are some of the projects I have taken part in
           </p>
         </div>
         
